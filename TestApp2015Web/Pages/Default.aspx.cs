2ec157e517ce75aaf522812dc644e7ba36bf2335
WBPrem <psekar2@worldbank.org>
2015-12-19 04:16:21 -0500

    Test One

--- TestApp2015Web/Pages/Default.aspx.cs
+++ TestApp2015Web/Pages/Default.aspx.cs
@@ -15,7 +15,7 @@ namespace TestApp2015Web
             switch (SharePointContextProvider.CheckRedirectionStatus(Context, out redirectUrl))
             {
                 //Test Jayaseelan
-                //Changes done by Prem Test 1
+                //Changes done by Prem Test 2
                 case RedirectionStatus.Ok:
                     return; 
                 case RedirectionStatus.ShouldRedirect:
